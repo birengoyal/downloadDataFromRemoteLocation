e531abe965f996cec35f176043a63af58b229b4e
Biren Goyal <biren.goyal@snapdeal.com>
2017-02-28 21:48:52 +0530

    first commit

---
+++ src/main/java/com/agoda/filedownloader/constants/URIConstants.java
@@ -0,0 +1,10 @@
+package com.agoda.filedownloader.constants;
+
+public interface URIConstants {
+
+   public static final String FIXED_FOLDER = "/home/local/JASPERINDIA/biren.goyal/Music/";
+   public static final String PROTOCOL = "/";
+   
+    
+   
+}
